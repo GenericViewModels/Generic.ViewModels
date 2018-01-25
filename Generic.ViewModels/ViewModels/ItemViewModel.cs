441a5db7e295d3294c069d618eadf33150437090
Christian Nagel <christian@christiannagel.com>
2018-01-25 13:45:17 +0100

    common library for generic view-models

---
+++ Generic.ViewModels/ViewModels/ItemViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericViewModels.ViewModels
+{
+    /// <summary>
+    /// base class for view-models that shows a single item
+    /// </summary>
+    /// <typeparam name="T">Item type for the view-model to display</typeparam>
+    public abstract class ItemViewModel<T> : ViewModelBase, IItemViewModel<T>
+    {
+        private T _item;
+        public virtual T Item
+        {
+            get => _item;
+            set => Set(ref _item, value);
+        }
+    }
+}
