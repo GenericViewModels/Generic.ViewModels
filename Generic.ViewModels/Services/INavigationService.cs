e683903e2d85dd9c942e0a415d7cddb68c7e3cb0
Christian Nagel <christian@christiannagel.com>
2018-01-27 10:08:01 +0100

    CanGoBack added to INavigationService

--- Generic.ViewModels/Services/INavigationService.cs
+++ Generic.ViewModels/Services/INavigationService.cs
@@ -5,6 +5,7 @@ namespace GenericViewModels.Services
     public interface INavigationService
     {
         Task NavigateToAsync(string page);
+        bool CanGoBack { get; }
         Task GoBackAsync();
         string CurrentPage { get; }
     }
