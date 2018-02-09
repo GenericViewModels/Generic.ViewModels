64d3e23cdbc7c2cdff95eee42ad4f6492a7da1d1
Christian Nagel <christian@christiannagel.com>
2018-02-09 10:56:50 +0100

    extensions and http helper

---
+++ Generic.ViewModels/Extensions/DateTimeOffsetExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericViewModels.Extensions
+{
+    public static class DateTimeOffsetExtensions
+    {
+        public static DateTimeOffset GetNextMonday(this DateTimeOffset date)
+        {
+            int addDays = 8 - (int)date.DayOfWeek;
+            if (addDays > 7) addDays -= 7;
+            return date.AddDays(addDays);
+        }
+
+        public static DateTimeOffset GetMonday(this DateTimeOffset date)
+        {
+            int number = (int)date.DayOfWeek;
+            if (number == 0) return date.AddDays(1);
+            else if (number == 1) return date;
+            else return date.AddDays(-(number - 1));
+        }
+    }
+}
---
+++ Generic.ViewModels/Net/IHttpHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericViewModels.Net
+{
+    public interface IHttpHelper<T>
+    {
+        Task<T> GetItemAsync(string url);
+        Task<IEnumerable<T>> GetItemsAsync(string url);
+        Task<T> AddItemAsync(string url, T item);
+        Task UpdateItemAsync(string url, T item);
+        Task<T> DeleteItemAsync(string url);
+    }
+}
