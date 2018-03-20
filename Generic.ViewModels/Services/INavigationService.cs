e240b85563d30f0b79a93a3040080edcfd83d016
Christian Nagel <christian@christiannagel.com>
2018-03-20 10:45:59 +0100

    re-add navigation

---
+++ Generic.ViewModels/Services/INavigationService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace GenericViewModels.Services
+{
+    public interface INavigationService
+    {
+        Task NavigateToAsync(string page);
+        bool CanGoBack { get; }
+        Task GoBackAsync();
+        string CurrentPage { get; }
+    }
+}
