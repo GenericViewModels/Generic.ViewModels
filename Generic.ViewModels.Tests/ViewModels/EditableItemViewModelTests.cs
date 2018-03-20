33fb621898b10a949b601cc5410e3d7cdea5a8b8
Christian Nagel <christian@christiannagel.com>
2018-03-20 10:45:33 +0100

    add unit tests

---
+++ Generic.ViewModels.Tests/ViewModels/EditableItemViewModelTests.cs
@@ -0,0 +1,159 @@
+﻿using GenericViewModels.Services;
+using GenericViewModels.ViewModels;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Generic.ViewModels.Tests.ViewModels
+{
+    public class EditableItemViewModelTests
+    {
+        public class AnItem
+        {
+            public string Text { get; set; }
+        }
+        public class TestEditableItemViewModel : EditableItemViewModel<AnItem>
+        {
+            public TestEditableItemViewModel(IItemsService<AnItem> itemsService)
+                : base(itemsService)
+            {
+            }
+
+            public override AnItem CreateCopy(AnItem item) => new AnItem() { Text = item?.Text ?? "empty" };
+            protected override void OnAdd() => throw new NotImplementedException();
+            public override Task OnSaveAsync()
+            {
+                Item = EditItem;
+                return Task.CompletedTask;
+            }
+        }
+
+        public EditableItemViewModelTests()
+        {
+            ObservableCollection<AnItem> items = new ObservableCollection<AnItem>
+            {
+                new AnItem { Text = "first" },
+                new AnItem { Text = "second" },
+                new AnItem { Text = "thirs" }
+            };
+            var mockingObject = new Mock<IItemsService<AnItem>>();
+            mockingObject.Setup(service => service.RefreshAsync())
+                .Returns(Task.CompletedTask);
+            mockingObject.Setup(service => service.Items)
+                .Returns(() => items);
+            mockingObject.Setup(service => service.SelectedItem)
+                .Returns(items[1]);
+            _itemsService = mockingObject.Object;
+        }
+
+        private IItemsService<AnItem> _itemsService;
+
+
+        [Fact]
+        public void IsEditModeRaiseEvents()
+        {
+            // arrange               
+            var viewModel = new TestEditableItemViewModel(_itemsService);
+            viewModel.IsEditMode = false;
+            bool cancelCommandFired = false;
+            bool saveCommandFired = false;
+            bool editCommandFired = false;
+
+            viewModel.CancelCommand.CanExecuteChanged += (sender, e) =>
+                cancelCommandFired = true;
+            viewModel.SaveCommand.CanExecuteChanged += (sender, e) =>
+                saveCommandFired = true;
+            viewModel.EditCommand.CanExecuteChanged += (sender, e) =>
+                editCommandFired = true;
+            // act
+            viewModel.IsEditMode = true;
+
+            // assert
+            Assert.True(viewModel.IsEditMode);
+            Assert.True(cancelCommandFired);
+            Assert.True(saveCommandFired);
+            Assert.True(editCommandFired);
+        }
+
+        [Fact]
+        public void FireEditItemEventOnItemChange()
+        {
+            bool editItemChangedFired = false;
+            bool itemChangedFired = false;
+
+            var viewModel = new TestEditableItemViewModel(_itemsService);
+            viewModel.PropertyChanged += (sender, e) =>
+            {
+                if (e.PropertyName == "Item") itemChangedFired = true;
+                if (e.PropertyName == "EditItem") editItemChangedFired = true;
+            };
+            viewModel.Item = new AnItem();
+
+            Assert.True(editItemChangedFired);
+            Assert.True(itemChangedFired);
+        }
+
+        [Fact]
+        public void BeginEdit_IsEditMode()
+        {
+            var viewModel = new TestEditableItemViewModel(_itemsService);
+
+            viewModel.BeginEdit();
+
+            Assert.True(viewModel.IsEditMode);
+        }
+
+        [Fact]
+        public void BeginEdit_CreateCopy()
+        {
+            var viewModel = new TestEditableItemViewModel(_itemsService);
+
+            viewModel.BeginEdit();
+            viewModel.EditItem.Text = "new text";
+
+            Assert.NotEqual(viewModel.Item.Text, viewModel.EditItem.Text);
+        }
+
+        [Fact]
+        public void CancelEdit_EditMode()
+        {
+            var viewModel = new TestEditableItemViewModel(_itemsService);
+            viewModel.BeginEdit();
+
+            viewModel.CancelEdit();
+
+            Assert.False(viewModel.IsEditMode);
+        }
+
+        [Fact]
+        public void CancelEdit_EditItemReset()
+        {
+            var viewModel = new TestEditableItemViewModel(_itemsService);
+            viewModel.BeginEdit();
+            viewModel.EditItem.Text = "new text";
+
+            viewModel.CancelEdit();
+
+            Assert.Equal("second", viewModel.Item.Text);
+            Assert.Equal(viewModel.Item.Text, viewModel.EditItem.Text);
+        }
+
+        [Fact]
+        public void EndEdit_ItemSaved()
+        {
+            var viewModel = new TestEditableItemViewModel(_itemsService);
+            viewModel.BeginEdit();
+            viewModel.EditItem.Text = "new text";
+
+            viewModel.EndEdit();
+
+            Assert.Equal("new text", viewModel.EditItem.Text);
+            Assert.Equal(viewModel.EditItem.Text, viewModel.Item.Text);
+        }
+    }
+}
---
+++ Generic.ViewModels.Tests/ViewModels/MasterDetailViewModelTests.cs
@@ -0,0 +1,125 @@
+﻿using GenericViewModels.Services;
+using GenericViewModels.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Generic.ViewModels.Tests.ViewModels
+{
+    public class MasterDetailViewModelTests
+    {
+        public class AnItem
+        {
+            public string Text { get; set; }
+        }
+        public class AnItemViewModel : ItemViewModel<AnItem>
+        {
+
+        }
+
+        class MockItemsService : IItemsService<AnItem>
+        {
+            public MockItemsService(IEnumerable<AnItem> items)
+            {
+                Items = new ObservableCollection<AnItem>(items);
+            }
+            public ObservableCollection<AnItem> Items { get; }
+
+            public AnItem SelectedItem { get; set; }
+
+            public event EventHandler<AnItem> SelectedItemChanged;
+
+            public Task<AnItem> AddOrUpdateAsync(AnItem item) => throw new NotImplementedException();
+            public Task DeleteAsync(AnItem item) => throw new NotImplementedException();
+            public Task RefreshAsync() => Task.CompletedTask;
+        }
+
+        public class TestMasterDetailViewModel : MasterDetailViewModel<AnItemViewModel, AnItem>
+        {
+            public TestMasterDetailViewModel(IItemsService<AnItem> itemsService)
+                : base(itemsService)
+            {
+            }
+
+            public override void OnAdd() => throw new NotImplementedException();
+            protected override AnItemViewModel ToViewModel(AnItem item) => new AnItemViewModel() { Item = item };
+        }
+
+        public MasterDetailViewModelTests()
+        {
+            _itemsService = new MockItemsService(new List<AnItem>() { _item1, _item2, _item3 });
+            _itemsService.SelectedItem = _item2;
+        }
+
+        private AnItem _item1 = new AnItem { Text = "first" };
+        private AnItem _item2 = new AnItem { Text = "second" };
+        private AnItem _item3 = new AnItem { Text = "third" };
+        private IItemsService<AnItem> _itemsService;
+
+        [Fact]
+        public void SetSelectedItem_RaiseEvents()
+        {
+            var viewModel = new TestMasterDetailViewModel(_itemsService);
+            bool selectedItemChangedFired = false;
+            bool selectedItemViewModelChangedFired = false;
+            viewModel.PropertyChanged += (sender, e) =>
+            {
+                if (e.PropertyName == "SelectedItem") selectedItemChangedFired = true;
+                if (e.PropertyName == "SelectedItemViewModel") selectedItemViewModelChangedFired = true;
+            };
+
+            viewModel.SelectedItem = _item1;
+
+            Assert.True(selectedItemChangedFired);
+            Assert.True(selectedItemViewModelChangedFired);
+        }
+
+        [Fact]
+        public void SetSelectedItem_DoNothing()
+        {
+            var viewModel = new TestMasterDetailViewModel(_itemsService);
+            bool selectedItemChangedFired = false;
+            bool selectedItemViewModelChangedFired = false;
+            viewModel.PropertyChanged += (sender, e) =>
+            {
+                if (e.PropertyName == "SelectedItem") selectedItemChangedFired = true;
+                if (e.PropertyName == "SelectedItemViewModel") selectedItemViewModelChangedFired = true;
+            };
+
+            viewModel.SelectedItem = _item2;
+
+            Assert.False(selectedItemChangedFired);
+            Assert.False(selectedItemViewModelChangedFired);
+        }
+
+        [Fact]
+        public void GetSelectedItemViewModel()
+        {
+            var viewModel = new TestMasterDetailViewModel(_itemsService);
+
+            var vm = viewModel.SelectedItemViewModel;
+
+            Assert.Equal(_item2, vm.Item);
+        }
+
+        [Fact]
+        public void SetSelectedItemViewModel_SetSelectedItem()
+        {
+            var viewModel = new TestMasterDetailViewModel(_itemsService);
+            viewModel.SelectedItemViewModel = new AnItemViewModel() { Item = _item3 };
+
+            Assert.Equal(_item3, viewModel.SelectedItem);
+        }
+
+        [Fact]
+        public void OnRefresh_SetSelectedItem()
+        {
+            var viewModel = new TestMasterDetailViewModel(_itemsService);
+            viewModel.OnRefresh();
+
+            Assert.Equal(_item1, viewModel.SelectedItem);
+        }
+    }
+}
