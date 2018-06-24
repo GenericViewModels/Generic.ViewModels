2c0fddd77ca06aaca5275bd6bd0c4a08d0ac7398
Christian Nagel <christian@christiannagel.com>
2018-06-24 14:40:21 +0200

    add implementation for ISelectedItemService

---
+++ Generic.ViewModels/Services/SelectedItemService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GenericViewModels.Services
+{
+    public class SelectedItemService<T> : ISelectedItemService<T>
+    {
+        private T _selectedItem;
+        public T SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
+                _selectedItem = value;
+                SelectedItemChanged?.Invoke(this, _selectedItem);
+            }
+        }
+
+        public event EventHandler<T> SelectedItemChanged;
+    }
+}
