442af2292261f8969cbb50f62f6a94da54151f01
Christian Nagel <christian@christiannagel.com>
2018-06-24 16:24:37 +0200

    datetime extensions

---
+++ Generic.ViewModels.Tests/Extensions/DateTimeExtensionTest.cs
@@ -0,0 +1,65 @@
+﻿using GenericViewModels.Extensions;
+using System;
+using Xunit;
+
+
+namespace Generic.ViewModels.Tests.Extensions
+{
+    public class DateTimeExtensionTest
+    {
+        [Fact]
+        public void GetMondayUsingMonday()
+        {
+            DateTime date = new DateTime(2018, 2, 5);
+            DateTime expected = date;
+            DateTime actual = date.GetMonday();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetMondayUsingWednesday()
+        {
+            DateTime date = new DateTime(2018, 2, 7);
+            DateTime expected = new DateTime(2018, 2, 5);
+            DateTime actual = date.GetMonday();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetNextMondayUsingMonday()
+        {
+            DateTime date = new DateTime(2018, 2, 5);
+            DateTime expected = date.AddDays(7);
+            DateTime actual = date.GetNextMonday();
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(2018, 2, 4, 2018, 2, 5)]
+        [InlineData(2018, 2, 5, 2018, 2, 12)]
+        [InlineData(2018, 2, 6, 2018, 2, 12)]
+        [InlineData(2018, 2, 7, 2018, 2, 12)]
+        [InlineData(2018, 2, 8, 2018, 2, 12)]
+        public void GetNextMonday(int year, int month, int day, int expectedYear, int expectedMonth, int expectedDay)
+        {
+            DateTime date = new DateTime(year, month, day);
+            DateTime expected = new DateTime(expectedYear, expectedMonth, expectedDay);
+            DateTime actual = date.GetNextMonday();
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(2018, 2, 4, 2018, 2, 5)]
+        [InlineData(2018, 2, 5, 2018, 2, 12)]
+        [InlineData(2018, 2, 6, 2018, 2, 12)]
+        [InlineData(2018, 2, 7, 2018, 2, 12)]
+        [InlineData(2018, 2, 8, 2018, 2, 12)]
+        public void GetNextMonday2(int year, int month, int day, int expectedYear, int expectedMonth, int expectedDay)
+        {
+            DateTime date = new DateTime(year, month, day);
+            DateTime expected = new DateTime(expectedYear, expectedMonth, expectedDay);
+            DateTime actual = date.GetNextMonday();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Generic.ViewModels/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericViewModels.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime GetNextMonday(this DateTime date)
+        {
+            int addDays = 8 - (int)date.DayOfWeek;
+            if (addDays > 7) addDays -= 7;
+            return date.AddDays(addDays);
+        }
+
+        public static DateTime GetMonday(this DateTime date)
+        {
+            int number = (int)date.DayOfWeek;
+            if (number == 0) return date.AddDays(1);
+            else if (number == 1) return date;
+            else return date.AddDays(-(number - 1));
+        }
+    }
+}
