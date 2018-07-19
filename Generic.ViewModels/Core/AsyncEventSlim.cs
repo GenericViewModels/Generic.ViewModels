08f4b8739a778ebfc4a9d6c587555994c0d4a5be
Christian Nagel <christian@christiannagel.com>
2018-07-19 10:36:23 +0200

    add asynceventslim

---
+++ Generic.ViewModels/Core/AsyncEventSlim.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace GenericViewModels.Core
+{
+    /// <summary>
+    /// AsyncEventSlim allows non-blocking wait for a signal to be set
+    /// With the implementation, <see cref="ManualResetEventSlim"/> is used
+    /// </summary>
+    public class AsyncEventSlim
+    {
+        private ManualResetEventSlim _event = new ManualResetEventSlim(false);
+
+        /// <summary>
+        /// Sets the event
+        /// </summary>
+        public void Signal() => _event.Set();
+
+        /// <summary>
+        /// non-blocking wait for the event to be set
+        /// </summary>
+        /// <returns>a <see cref="Task"/> that completes when the event is set</returns>
+        public async Task WaitAsync()
+            => await Task.Run(() => _event.Wait());
+    }
+}
---
+++ Generic.ViewModels/Services/ItemsService.cs
@@ -0,0 +1,36 @@
+﻿using GenericViewModels.Core;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace GenericViewModels.Services
+{
+    public abstract class ItemsService<T> : IItemsService<T>
+    {
+        private AsyncEventSlim _initalized = new AsyncEventSlim();
+
+        public ItemsService()
+        {
+            Task t = InitAsync();
+        }
+
+        private async Task InitAsync()
+        {
+            try
+            {
+                await InitAsyncCore();
+            }
+            finally
+            {
+                _initalized.Signal();
+            }
+        }
+
+        protected Task InitAsyncCore() => Task.CompletedTask;
+
+        public abstract ObservableCollection<T> Items { get; }
+
+        public abstract Task<T> AddOrUpdateAsync(T item);
+        public abstract Task DeleteAsync(T item);
+        public abstract Task RefreshAsync();
+    }
+}
