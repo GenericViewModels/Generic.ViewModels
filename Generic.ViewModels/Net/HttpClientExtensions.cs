7f104f287676096621beda204d9daef5291f1226
Christian Nagel <christian@christiannagel.com>
2020-06-06 14:30:18 +0200

    change json serializer #7

--- Generic.ViewModels/Net/HttpClientExtensions.cs
+++ Generic.ViewModels/Net/HttpClientExtensions.cs
@@ -1,15 +1,20 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace GenericViewModels.Net
 {
     public static class HttpClientExtensions
     {
+        private static JsonSerializerOptions s_options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
         public static async Task<IEnumerable<T>> GetItemsAsync<T>(this HttpClient httpClient, string url)
         {
             if (httpClient == null) throw new ArgumentNullException(nameof(httpClient));
@@ -18,9 +23,7 @@ namespace GenericViewModels.Net
             HttpResponseMessage response = await httpClient.GetAsync(url);
             response.EnsureSuccessStatusCode();
             Stream stream = await response.Content.ReadAsStreamAsync();
-            using var reader = new StreamReader(stream);
-            using var jsonReader = new JsonTextReader(reader);
-            return new JsonSerializer().Deserialize<IEnumerable<T>>(jsonReader);
+            return await JsonSerializer.DeserializeAsync<IEnumerable<T>>(stream, s_options);
         }
 
         public static async Task<T> GetItemAsync<T>(this HttpClient httpClient, string url)
@@ -31,9 +34,7 @@ namespace GenericViewModels.Net
             HttpResponseMessage response = await httpClient.GetAsync(url);
             response.EnsureSuccessStatusCode();
             Stream stream = await response.Content.ReadAsStreamAsync();
-            using var reader = new StreamReader(stream);
-            using var jsonReader = new JsonTextReader(reader);
-            return new JsonSerializer().Deserialize<T>(jsonReader);
+            return await JsonSerializer.DeserializeAsync<T>(stream, s_options);
         }
 
         public static async Task<T> AddItemAsync<T>(this HttpClient httpClient, string url, T item)
@@ -42,14 +43,13 @@ namespace GenericViewModels.Net
             if (url == null) throw new ArgumentNullException(nameof(url));
             if (item == null) throw new ArgumentNullException(nameof(item));
 
-            string json = JsonConvert.SerializeObject(item);
+            string json = JsonSerializer.Serialize(item, s_options);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
             HttpResponseMessage response = await httpClient.PostAsync(url, content);
             response.EnsureSuccessStatusCode();
             Stream stream = await response.Content.ReadAsStreamAsync();
-            using var reader = new StreamReader(stream);
-            using var jsonReader = new JsonTextReader(reader);
-            return new JsonSerializer().Deserialize<T>(jsonReader);
+
+            return await JsonSerializer.DeserializeAsync<T>(stream, s_options);
         }
 
         public static async Task UpdateItemAsync<T>(this HttpClient httpClient, string url, T item)
@@ -58,7 +58,7 @@ namespace GenericViewModels.Net
             if (url == null) throw new ArgumentNullException(nameof(url));
             if (item == null) throw new ArgumentNullException(nameof(item));
 
-            string json = JsonConvert.SerializeObject(item);
+            string json = JsonSerializer.Serialize(item, s_options);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
             HttpResponseMessage response = await httpClient.PutAsync(url, content);
             response.EnsureSuccessStatusCode();
@@ -72,9 +72,7 @@ namespace GenericViewModels.Net
             HttpResponseMessage response = await httpClient.DeleteAsync(url);
             response.EnsureSuccessStatusCode();
             Stream stream = await response.Content.ReadAsStreamAsync();
-            using var reader = new StreamReader(stream);
-            using var jsonReader = new JsonTextReader(reader);
-            return new JsonSerializer().Deserialize<T>(jsonReader);
+            return await JsonSerializer.DeserializeAsync<T>(stream, s_options);
         }
     }
 }
