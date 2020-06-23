221d298d6e652e38aafc679353a54760acbe87e1
Christian Nagel <christian@christiannagel.com>
2020-06-23 11:01:55 +0200

    change from field to auto prop

--- Generic.ViewModels/ViewModels/MasterDetailViewModel.cs
+++ Generic.ViewModels/ViewModels/MasterDetailViewModel.cs
@@ -14,10 +14,8 @@ namespace GenericViewModels.ViewModels
         where TItemViewModel : class, IItemViewModel<TItem>
         where TItem : class
     {
-        private readonly IItemsService<TItem> _itemsService;
-        private readonly ILogger _logger;
-        protected IItemsService<TItem> ItemsService => _itemsService;
-        protected ILogger Logger => _logger;
+        protected IItemsService<TItem> ItemsService { get; }
+        protected ILogger Logger { get; }
 
         private readonly IItemToViewModelMap<TItem, TItemViewModel> _viewModelMap;
 
@@ -28,26 +26,26 @@ namespace GenericViewModels.ViewModels
             ILoggerFactory loggerFactory)
             : base(showProgressInfo)
         {
-            _itemsService = itemsService ?? throw new ArgumentNullException(nameof(itemsService));
+            ItemsService = itemsService ?? throw new ArgumentNullException(nameof(itemsService));
             _viewModelMap = viewModelMap ?? throw new ArgumentNullException(nameof(viewModelMap));
-            _logger = loggerFactory?.CreateLogger(GetType()) ?? throw new ArgumentNullException(nameof(loggerFactory));
+            Logger = loggerFactory?.CreateLogger(GetType()) ?? throw new ArgumentNullException(nameof(loggerFactory));
 
-            _itemsService.SelectedItemChanged += ItemsService_SelectedItemChanged;
+            ItemsService.SelectedItemChanged += ItemsService_SelectedItemChanged;
 
-            _itemsService.Items.CollectionChanged += (sender, e) =>
+            ItemsService.Items.CollectionChanged += (sender, e) =>
             {
                 RaisePropertyChanged(nameof(Items));
             };
 
-            _itemsService.PropertyChanged += ItemsService_PropertyChanged;
+            ItemsService.PropertyChanged += ItemsService_PropertyChanged;
         }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
-                _itemsService.SelectedItemChanged -= ItemsService_SelectedItemChanged;
-                _itemsService.PropertyChanged -= ItemsService_PropertyChanged;
+                ItemsService.SelectedItemChanged -= ItemsService_SelectedItemChanged;
+                ItemsService.PropertyChanged -= ItemsService_PropertyChanged;
             }
         }
 
@@ -64,7 +62,7 @@ namespace GenericViewModels.ViewModels
 
         private void ItemsService_SelectedItemChanged(object sender, SelectedItemEventArgs<TItem> e)
         {
-            _logger.LogTrace(LoggingMessages.SelectedItemChanged(typeof(MasterDetailViewModel<TItemViewModel, TItem>), e.Item));
+            Logger.LogTrace(LoggingMessages.SelectedItemChanged(typeof(MasterDetailViewModel<TItemViewModel, TItem>), e.Item));
             RaisePropertyChanged(nameof(SelectedItem));
         }
 
@@ -78,20 +76,20 @@ namespace GenericViewModels.ViewModels
 
         public virtual TItemViewModel? SelectedItem
         {
-            get => ToViewModel(_itemsService.SelectedItem);
+            get => ToViewModel(ItemsService.SelectedItem);
             set
             {
-                _logger.LogTrace($"SelectedItem updating to item {value?.Item}");
+                Logger.LogTrace($"SelectedItem updating to item {value?.Item}");
                 if (value?.Item != null)
                 {
-                    _itemsService.SetSelectedItem(value.Item);
+                    ItemsService.SetSelectedItem(value.Item);
                 }
             }
         }
 
         protected async Task RefreshAsync()
         {
-            _logger.LogTrace(LoggingMessages.Refresh(typeof(MasterDetailViewModel<TItemViewModel, TItem>)));
+            Logger.LogTrace(LoggingMessages.Refresh(typeof(MasterDetailViewModel<TItemViewModel, TItem>)));
 
             using var progress = ShowProgressInfo.StartInProgress(ProgressInfoName);
             await OnRefreshCoreAsync();
@@ -103,7 +101,7 @@ namespace GenericViewModels.ViewModels
         /// <returns>> <see cref="Task"/></returns>
         protected virtual async Task OnRefreshCoreAsync()
         {
-            await _itemsService.RefreshAsync();
+            await ItemsService.RefreshAsync();
         }
 
         /// <summary>
